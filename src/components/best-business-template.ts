dcc2fe83e6763a445f7ba4fc7e40b52d74535117
David Pérez <taborlindp@gmail.com>
2024-09-11 14:11:09 +0200

    install tailwind

--- src/components/best-business-template.ts
+++ src/components/best-business-template.ts
@@ -1,20 +1,23 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
+import { TWStyles } from '../styles/tw.js';
 
-const logo = new URL('./assets/mermaid.png', import.meta.url).href;
+const logo = new URL('../assets/mermaid.png', import.meta.url).href;
 
 @customElement('best-business-template')
 export class BestBusinessTemplate extends LitElement {
   @property({ type: String }) title = 'Mejores lorem ipsum en Benidorm';
 
-  static styles = css`
-  `;
+  static styles = [css ``, TWStyles];
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
+        <div class="logo"><img alt="mermaid" src=${logo} /></div>
         <h1>${this.title}</h1>
+        <div class="flex flex-row bg-red-500">
+          This is my element
+        </div>
       </main>
 
       <p class="app-footer">
